edbfc54537d9a69b223a2953c461d9caebafe30f
Metric <134078516+mjasil@users.noreply.github.com>
2026-06-26 00:30:01 +0530

    Add frontend/src/lib/store.ts

---
+++ frontend/src/lib/store.ts
@@ -0,0 +1,27 @@
+import { create } from 'zustand'
+
+interface Store {
+  user: any
+  token: string | null
+  selectedAccount: number
+  setUser: (user: any) => void
+  setToken: (token: string) => void
+  setSelectedAccount: (n: number) => void
+  logout: () => void
+}
+
+export const useStore = create<Store>((set) => ({
+  user: null,
+  token: null,
+  selectedAccount: 1,
+  setUser: (user) => set({ user }),
+  setToken: (token) => {
+    localStorage.setItem('carlos_token', token)
+    set({ token })
+  },
+  setSelectedAccount: (n) => set({ selectedAccount: n }),
+  logout: () => {
+    localStorage.removeItem('carlos_token')
+    set({ user: null, token: null })
+  }
+}))
