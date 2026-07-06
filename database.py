f49644961b097af9a3c6c281544afb0dc65b95ae
Metric <134078516+mjasil@users.noreply.github.com>
2026-07-06 15:15:00 +0530

    Update Supabase credentials

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 from supabase import create_client
 import os
 
-url = os.getenv("SUPABASE_URL")
-key = os.getenv("SUPABASE_KEY")
+url = os.getenv("SUPABASE_URL", "https://owqtmdflhykxildcrpvc.supabase.co")
+key = os.getenv("SUPABASE_KEY", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im93cXRtZGZsaHlreGlsZGNycHZjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMzMjc5MDUsImV4cCI6MjA5ODkwMzkwNX0.P8iTPSIuYfBJsZuV3UgGmrfpGP2p8aDnFH_NxtmTNjo")
 supabase = create_client(url, key)
